6cc981d7afbdc1fd7bb9734ad42defab66445af6
Mukhammadali Dadabaev <ali_6017@mail.ru>
2022-11-06 08:52:10 +0500

    practice modal-card-link refactore

--- src/components/PlayListContextMenuItem.js
+++ src/components/PlayListContextMenuItem.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+// react-icons
 import { FiChevronRight } from "react-icons/fi";
 import PlayListContextMenu from "./PlayListContextMenu";
 
 function PlayListContextMenuItem({ children: label, subMenuItems }) {
+  // STATE
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuPositionClass, setMenuPositionClass] = useState("left-full");
+  // Ref
+  const menuItemRef = useRef(null);
+
+  // Modal-Link
+  function getMenuPositionClass() {
+    const menuItem = menuItemRef.current;
+    const menuWidth = menuItem.offsetWidth;
+    const windowWidth = window.innerWidth;
+    const menuItemEndCoordinate = menuItem.getBoundingClientRect().right;
+    const shouldMoveMenuLeft = menuWidth > windowWidth - menuItemEndCoordinate;
+
+    return shouldMoveMenuLeft ? "right-full" : "left-full";
+  }
+
+  function openMenu() {
+    setIsMenuOpen(true);
+    setMenuPositionClass(getMenuPositionClass);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   if (subMenuItems) {
     return (
-      <li className="relative">
-        <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center peer">
+      <li
+        className="relative"
+        onMouseEnter={openMenu}
+        onMouseLeave={closeMenu}
+        ref={menuItemRef}
+      >
+        <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center">
           {label} <FiChevronRight className="h-4 w-4" />
         </button>
 
-        <PlayListContextMenu
-          menuItems={subMenuItems}
-          classes="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default invisible peer-hover:visible hover:visible"
-        />
+        {isMenuOpen && (
+          <PlayListContextMenu
+            menuItems={subMenuItems}
+            classes={`bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default absolute top-0 ${menuPositionClass}`}
+          />
+        )}
       </li>
     );
   }
